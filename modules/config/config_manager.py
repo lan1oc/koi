2ce16dc13b2036771d77944f51dd4ab8b6bb36aa
lan1oc <lan1oc@qq.com>
2026-03-12 10:43:34 +0800

    重构冗余逻辑

--- modules/config/config_manager.py
+++ modules/config/config_manager.py
@@ -106,7 +106,7 @@ class ConfigManager:
                 'last_updated': ''
             },
             'app': {
-                'version': '2.3.7',
+                'version': '2.4.1',
                 'first_run': True,
                 'last_updated': ''
             },
